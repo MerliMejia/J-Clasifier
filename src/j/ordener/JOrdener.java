917bf220c9c8ba891103cb614c294e9eaae0db1d
Merli Mejia <merlimejia25@gmail.com>
2018-12-21 20:36:24 -0400

    Finish v1.0

--- src/j/ordener/JOrdener.java
+++ src/j/ordener/JOrdener.java
@@ -1,15 +1,26 @@
 
 package j.ordener;
+import com.bulenkov.darcula.DarculaLaf;
 import j.ordener.archivos.excel;
 import java.io.File;
-import javax.swing.JFileChooser;
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class JOrdener {
 
 
     public static void main(String[] args) {
-        JFileChooser fc = new JFileChooser("D:\\Dropbox\\Proyectos Personales\\JAVA\\J-Ordener");
+
+        BasicLookAndFeel darcula = new DarculaLaf();
+        try {
+            UIManager.setLookAndFeel(darcula);
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
         int aceptar = fc.showOpenDialog(null);
         if(aceptar == JFileChooser.APPROVE_OPTION)
         {
--- src/j/ordener/archivos/excel.java
+++ src/j/ordener/archivos/excel.java
@@ -1,10 +1,7 @@
 
 package j.ordener.archivos;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,11 +14,15 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import javax.swing.*;
+
 public class excel {
-    
+    static Workbook workbook;
     public static void ordenarArchivo(File archivo)
     {
-        Sheet hoja = ordenar(lector(archivo));
+        Sheet hojaxD = ordenar(lector(archivo), archivo);
+        Sheet hoja = workbook.getSheetAt(0);
+
         //System.out.println(hoja.getPhysicalNumberOfRows());
         for(int i = 0; i < hoja.getPhysicalNumberOfRows(); i++)
         {
@@ -29,20 +30,48 @@ public class excel {
             {
                 if(hoja.getRow(i).getCell(a).getCellType() == Cell.CELL_TYPE_STRING)
                 {
-                    System.out.println(hoja.getRow(i).getCell(a).getStringCellValue());
+                    //System.out.println(hoja.getRow(i).getCell(a).getStringCellValue());
                 }else if(hoja.getRow(i).getCell(a).getCellType() == Cell.CELL_TYPE_NUMERIC)
                 {
-                    System.out.println(hoja.getRow(i).getCell(a).getNumericCellValue());
+                    //System.out.println(hoja.getRow(i).getCell(a).getNumericCellValue());
                 }
             }
         }
-        
+
+        FileOutputStream fileOut = null;
+        JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        int aceptar = fc.showOpenDialog(null);
+        if(aceptar == JFileChooser.APPROVE_OPTION)
+        {
+            String nombre = fc.getSelectedFile().getPath() + "\\" + archivo.getName().replace(".xls", "");
+            try {
+                fileOut = new FileOutputStream(nombre + " DEPURADO.xls");
+                workbook.write(fileOut);
+                fileOut.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //System.out.println(hoja.getRow(7).getCell(0));
+        }else
+        {
+
+
+        }
+
+
+
+
     }
     
     public static Sheet lector(File archivo)
     {
         try {
-            Workbook workbook = new HSSFWorkbook(new FileInputStream(archivo));
+            workbook = new HSSFWorkbook(new FileInputStream(archivo));
             Sheet hoja = workbook.getSheetAt(0);
             return hoja;
         } catch (FileNotFoundException ex) {
@@ -54,7 +83,7 @@ public class excel {
         return null;
     }
     
-    public static Sheet ordenar(Sheet hoja)
+    public static Sheet ordenar(Sheet hoja, File archivo)
     {
         Sheet hojaOrdenada = hoja;
         
@@ -67,6 +96,9 @@ public class excel {
        
         ArrayList<String> datos = new ArrayList<>();
         Map<Integer, ArrayList> mapa = new HashMap<>();
+
+        ArrayList<String> datos2 = new ArrayList<>();
+        Map<Integer, ArrayList> mapa2 = new HashMap<>();
         
         
        
@@ -75,15 +107,23 @@ public class excel {
             datos = new ArrayList<>();
             for(int a = 1; a < hoja.getRow(i).getPhysicalNumberOfCells(); a++)
             {
-                if(hoja.getRow(i).getCell(a).getCellType() == Cell.CELL_TYPE_STRING)
-                {
-                    datos.add(hoja.getRow(i).getCell(a).getStringCellValue());
-                }else
-                {
-                    datos.add(Double.toString(hoja.getRow(i).getCell(a).getNumericCellValue()));
-                }
+
+                hoja.getRow(i).getCell(a).setCellType(Cell.CELL_TYPE_STRING);
+                datos.add(hoja.getRow(i).getCell(a).getStringCellValue());
+
             }
             mapa.put(i, datos);
+        }
+
+        for(int i = 36; i < 60; i++)
+        {
+            datos2 = new ArrayList<>();
+            for(int a = 1; a < hoja.getRow(i).getPhysicalNumberOfCells(); a++)
+            {
+                hoja.getRow(i).getCell(a).setCellType(Cell.CELL_TYPE_STRING);
+                datos2.add(hoja.getRow(i).getCell(a).getStringCellValue());
+            }
+            mapa2.put(i, datos2);
         }
         
         int[] indices = new int[]{
@@ -115,21 +155,79 @@ public class excel {
             31,
             32
         };
+
+        int[] indices2 = new int[]{
+
+                36,
+                46,
+                38,
+                44,
+                39,
+                43,
+                45,
+                53,
+                48,
+                51,
+                49,
+                54,
+                57,
+                59,
+                52,
+                55,
+                56,
+                37,
+                40,
+                41,
+                42,
+                50,
+                47,
+                58,
+                60
+        };
         
         for(int i = 8; i < 34; i++)
         {
             //System.out.println(mapa.get(i).size());
-            for(int a = 0; a < mapa.get(i).size(); a++)
+            for(int a = 1; a < mapa.get(i).size(); a++)
             {
-                hojaOrdenada.getRow(i).getCell(a).setCellValue((String) mapa.get(indices[i - 8]).get(a));
+                System.out.println((String) mapa.get(indices[i - 8]).get(a-1));
+                try
+                {
+                    hoja.getRow(i).getCell(a).setCellValue(Double.parseDouble((String) mapa.get(indices[i - 8]).get(a-1)));
+
+                }catch (Exception e)
+                {
+                    hoja.getRow(i).getCell(a).setCellValue((String) mapa.get(indices[i - 8]).get(a-1));
+                }
                 
             }
             
             
         }
-        
-        return hojaOrdenada;
+
+        for(int i = 36; i < 60; i++)
+        {
+            //System.out.println(mapa.get(i).size());
+            for(int a = 1; a < mapa2.get(i).size(); a++)
+            {
+                System.out.println((String) mapa2.get(indices2[i - 36]).get(a-1));
+                try
+                {
+                    hoja.getRow(i).getCell(a).setCellValue(Double.parseDouble((String) mapa2.get(indices2[i - 36]).get(a-1)));
+
+                }catch (Exception e)
+                {
+                    hoja.getRow(i).getCell(a).setCellValue((String) mapa2.get(indices2[i - 36]).get(a-1));
+                }
+
+
+            }
+
+
+        }
+
+
+        return hoja;
     }
-    
     
 }
